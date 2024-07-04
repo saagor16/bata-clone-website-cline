490e953cef5256a46435e49ff83673406205b2d5
saagor16 <saagor.16@gmail.com>
2024-07-04 20:21:37 +0600

    banner seating

--- src/Container/HomePage/Home/Home.jsx
+++ src/Container/HomePage/Home/Home.jsx
@@ -1,15 +1,15 @@
+import Banner from "../Banner/Banner";
 import Gallery from "../Gallery/Gallery";
 import Slider from "../Slider/Slider";
 
 const Home = () => {
-    return (
-        <div>
-            <Slider></Slider>
-            <div>
-                <Gallery></Gallery>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Slider></Slider>
+      <Gallery></Gallery>
+      <Banner></Banner>
+    </div>
+  );
 };
 
 export default Home;
