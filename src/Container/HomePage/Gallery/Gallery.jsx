b7a9a29fcd76331d42bfebc5e08f3b1836e0c44f
saagor16 <saagor.16@gmail.com>
2024-07-05 20:12:56 +0600

    Banner section

--- src/Container/HomePage/Gallery/Gallery.jsx
+++ src/Container/HomePage/Gallery/Gallery.jsx
@@ -8,22 +8,22 @@ const Gallery = () => {
           <div className="container justify-center p-4 mx-auto">
             <div className="grid gap-4 grid-cols-2 md:grid-cols-4 ">
               <img
-                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px] rounded-md duration-200 hover:scale-105"
+                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px] duration-200 hover:scale-105"
                 src={art}
                 alt="Gallery Image 1"
               />
               <img
-                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px] rounded-md duration-200 hover:scale-105"
+                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px] duration-200 hover:scale-105"
                 src={art}
                 alt="Gallery Image 2"
               />
               <img
-                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px] rounded-md duration-200 hover:scale-105"
+                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px]  duration-200 hover:scale-105"
                 src={art}
                 alt="Gallery Image 3"
               />
               <img
-                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px] rounded-md duration-200 hover:scale-105"
+                className="object-cover w-full dark:bg-gray-500 lg:w-[300px] lg:h-[197px] md:w-[170px] md:h-[90px] sm:w-[120px] sm:h-[70px] duration-200 hover:scale-105"
                 src={art}
                 alt="Gallery Image 4"
               />
