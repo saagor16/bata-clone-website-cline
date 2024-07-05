0bf6dae63a559827068e9fbb19fb590e1810e77b
saagor16 <saagor.16@gmail.com>
2024-07-05 10:09:46 +0600

    Banner section done

--- src/Container/HomePage/Banner/Banner.jsx
+++ src/Container/HomePage/Banner/Banner.jsx
@@ -1,8 +1,10 @@
 
+import art from "../../../assets/Banner/real.png"
+
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="container mx-auto">
+            <img className="lg:h-[360px] w-full" src={art} alt="" />
         </div>
     );
 };
