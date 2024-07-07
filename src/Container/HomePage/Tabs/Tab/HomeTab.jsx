91efed36b44e199207f61f59ef1d2491c063c73a
saagor16 <saagor.16@gmail.com>
2024-07-07 16:29:38 +0600

    casuals add tabs

--- src/Container/HomePage/Tabs/Tab/HomeTab.jsx
+++ src/Container/HomePage/Tabs/Tab/HomeTab.jsx
@@ -4,8 +4,8 @@ import Casuals from '../../../../AllTabs/Casuals/Casuals';
 
 const HomeTab = () => {
     return (
-        <Tabs className='container mx-auto mt-10 px-4 sm:px-6 lg:px-8'>
-            <h2 className="text-lg font-bold mb-4 ">JUST LANDED</h2>
+        <Tabs className="container mx-auto mt-10 px-4 sm:px-6 lg:px-8">
+            <h2 className="text-lg font-bold mb-4">JUST LANDED</h2>
             <TabList className="flex space-x-4 pt-5 overflow-x-auto justify-center scrollbar-hide">
                 <Tab className="px-4 py-2 cursor-pointer hover:bg-gray-100 focus:outline-none" selectedClassName="bg-gray-200 rounded">Casuals</Tab>
                 <Tab className="px-4 py-2 cursor-pointer hover:bg-gray-100 focus:outline-none" selectedClassName="bg-gray-200 rounded">Sneakers</Tab>
@@ -15,11 +15,9 @@ const HomeTab = () => {
                 <Tab className="px-4 py-2 cursor-pointer hover:bg-gray-100 focus:outline-none" selectedClassName="bg-gray-200 rounded">Accessories</Tab>
             </TabList>
     
-            <div className='mt-10'>
+            <div className="mt-10">
                 <TabPanel>
-                    <div>
-                        <Casuals></Casuals>
-                    </div>
+                    <Casuals />
                 </TabPanel>
                 <TabPanel>
                     <h2>Sneakers content</h2>
